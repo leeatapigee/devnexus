bfad2914f9aa14a051b3d3517fa881dcbafcf394
leeatapigee <ApigeeCorporation@Lee-Greys-Macbook.local>
2015-03-06 14:25:57 -0500

    Added basis for web pages, as well as first half of mashup API

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-'use strict';
+//'use strict';
 
 var a127 = require('a127-magic');
 var express = require('express');
 var app = express();
 
+
+////////////////////////////////////////////////////////////////////////////////
+// api stuff
+
 // uncomment the following if you need to parse incoming form data
-//app.use(express.bodyParser());
+app.use(express.bodyParser());
 
 a127.init(function(config) {
 
@@ -15,3 +19,16 @@ a127.init(function(config) {
 
   console.log('try this:\ncurl http://127.0.0.1:10010/hello?name=Scott');
 });
+
+////////////////////////////////////////////////////////////////////////////////
+// web stuff
+
+require('./router/main')(app)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile)
+
+
+var server = app.listen(3000, function() {
+  console.log('Express is listening on 3000')
+})
