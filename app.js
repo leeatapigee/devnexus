3ea803aa5ee6837acddea618fb607332633eb2d0
leeatapigee <ApigeeCorporation@Lee-Greys-Macbook.local>
2015-03-06 19:30:40 -0500

    Deployed to Edge

--- app.js
+++ app.js
@@ -2,33 +2,26 @@
 
 var a127 = require('a127-magic');
 var express = require('express');
+var ejs = require('ejs');
 var app = express();
 
 
-////////////////////////////////////////////////////////////////////////////////
-// api stuff
-
 // uncomment the following if you need to parse incoming form data
-app.use(express.bodyParser());
+//app.use(express.bodyParser());
+
 
 a127.init(function(config) {
 
   app.use(a127.middleware(config));
 
-  app.listen(process.env.PORT || 10010);
 
+  // web stuff
+  require('./router/main')(app)
+  app.set('views', __dirname + '/views')
+  app.set('view engine', 'ejs')
+  app.engine('html', require('ejs').renderFile)
+
+
+  app.listen(process.env.PORT || 10010);
   console.log('try this:\ncurl http://127.0.0.1:10010/hello?name=Scott');
 });
-
-////////////////////////////////////////////////////////////////////////////////
-// web stuff
-
-require('./router/main')(app)
-app.set('views', __dirname + '/views')
-app.set('view engine', 'ejs')
-app.engine('html', require('ejs').renderFile)
-
-
-var server = app.listen(3000, function() {
-  console.log('Express is listening on 3000')
-})
